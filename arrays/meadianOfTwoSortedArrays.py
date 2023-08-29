1da65fc483217a38b81124393f70caab15fef8a1
Haruna Ogweda <hogweda@gmail.com>
2023-08-29 11:49:27 -0500

    added new questions and solutions

---
+++ arrays/meadianOfTwoSortedArrays.py
@@ -0,0 +1,59 @@
+'''
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+'''
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n = len(nums1)
+        m = len(nums2)
+        result = []
+
+        i = j = 0
+
+        while i < n and j < m:
+            if nums1[i] < nums2[j]:
+                result.append(nums1[i])
+                i += 1
+            elif nums1[i] > nums2[j]:
+                result.append(nums2[j])
+                j += 1
+            else:
+                result.append(nums1[i])
+                i += 1
+                result.append(nums2[j])
+                j += 1
+
+        if i<n:
+            for num in nums1[i:]:
+                result.append(num)
+
+        if j<m:
+            for num in nums2[j:]:
+                result.append(num)
+
+        print(result)
+
+        n = len(result)
+
+        if n%2 == 0:
+            output = (result[int(n/2)-1] + result[int(n/2)])/2
+        else:
+            output = result[int((n+1)/2)-1]
+
+        return output
+
---
+++ linkedlists/addTwoNumbers.py
@@ -0,0 +1,77 @@
+'''
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        remainder = 0
+        
+        result = ListNode()
+        temp = result
+
+        temp1 = l1
+        temp2 = l2
+        while temp1 and temp2:
+            new_sum = temp1.val + temp2.val + remainder
+            if new_sum >= 10:
+                new_sum -= 10
+                remainder = 1
+            else:
+                remainder = 0
+            temp1 = temp1.next
+            temp2 = temp2.next
+            
+            temp.next = ListNode(new_sum)
+            temp = temp.next
+
+        while temp1:
+            new_sum = temp1.val + remainder
+            if new_sum >= 10:
+                new_sum -= 10
+                remainder = 1
+            else:
+                remainder = 0
+            temp1 = temp1.next
+            
+            temp.next = ListNode(new_sum)
+            temp = temp.next
+        
+        while temp2:
+            new_sum = temp2.val + remainder
+            if new_sum >= 10:
+                new_sum -= 10
+                remainder = 1
+            else:
+                remainder = 0
+            temp2 = temp2.next
+
+            temp.next = ListNode(new_sum)
+            temp = temp.next
+            
+        if remainder == 1:
+            temp.next = ListNode(remainder)
+           
+
+        return result.next
